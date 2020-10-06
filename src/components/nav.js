e3b488a52e001fe421927fb8377e41db6385dca0
heffoo <57847291+heffoo@users.noreply.github.com>
2020-10-06 17:59:09 +0300

    1commit

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,5 +1,4 @@
 import React from "react";
-import someForm from "../components/content";
 
 import "./nav.scss";
 
--- src/components/someForm.js
+++ src/components/someForm.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, {useRef, useEffect} from "react";
 
 import "../components/content.scss";
 
-const SomeForm = () => {
-  return <div className="someForm"></div>;
+const SomeForm = ({ setOpen, isOpen}) => {
+  const ssform = useRef(null);
+  useEffect(() => {
+
+    const onClickOutsideHandler = (e) => {
+      isOpen && !ssform.current.contains(e.target) && setOpen(false);
+    };
+
+    window.addEventListener("click", onClickOutsideHandler);
+    return () => window.removeEventListener("click", onClickOutsideHandler);
+  }, [setOpen, isOpen, ssform])
+
+  return <div ref = {ssform} className="someForm"></div>;
 };
+
 export default SomeForm;
