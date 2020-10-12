a340e19e4637064e91f906a3d34798428d15fbe0
heffoo <57847291+heffoo@users.noreply.github.com>
2020-10-12 17:56:06 +0300

    kkk

--- src/components/page/Blog/blog.js
+++ src/components/page/Blog/blog.js
@@ -7,12 +7,12 @@ const Blog = () => {
     <section className="mainContent5">
       <div className="img3" id="blog"></div>
       <div className="textContent5">
-          <p className='blogp'>  YOU SHOULD ALSO KNOWN    </p>
+          <h4>  YOU SHOULD ALSO KNOWN    </h4>
         <p >
           MDD affects more than 16.1 million American adults, or about 6.7%of the U.S. population age 18 and older in a
-          given year. We at ThriveTalk can help you.
+          given year. We at <span className="text-mark">ThriveTalk</span> can help you.
         </p>
-            <button>Get started now</button>
+            <button className="button-blog">GET HELP NOW</button>
         </div>
     </section>
   );
